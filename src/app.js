2640c56ffb1ca190a0b329d615cf9cfb687f871e
rough0peace <ianarthurgarvey@gmail.com>
2025-04-08 23:31:16 +0000

    First commit - made a list of domain names

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ['that', 'our', 'your', 'their']
+  let adjs = ['uptopian', 'dangerous', 'wacky', 'odd'];
+  let nouns = ['dumpster', 'spoon', 'violin', 'megaphone'];
+  const domains = document.querySelector("ul");
+
+  for(let i = 0; i < pronouns.length; i++) {
+    for(let j = 0; j < adjs.length; j++) {
+      for(let k = 0; k < nouns.length; k++) {
+        console.log(pronouns[i] + adjs[j] + nouns[k]);
+        let li = document.createElement("li");
+        li.textContent = pronouns[i] + adjs[j] + nouns[k];
+        domains.appendChild(li);
+      }
+    }
+  }
 };
