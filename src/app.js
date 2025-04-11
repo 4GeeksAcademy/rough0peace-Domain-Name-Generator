1eb1302ea158e6fe4224a93a6d371a59cf2ed1e1
rough0peace <ianarthurgarvey@gmail.com>
2025-04-11 01:31:12 +0000

    created 4 arrays. Deleted default body code, added h1 and ul. Grabbed the ul id with a queery selector and saved that in a variable called domains in the global scope. Was advised to use const as much as possible, so did that for webs. Created 3 nested for loops for each array. For each array, starting at the first item index 0, increases by 1 increment and returns the value and does so for the length of the array. I originally made a 4th nested for loop for the randomWeb variable, but it made it so that you had to refresh the page to see all the different website endings so I deleted that for loop and moved the randomWeb function? I think it's the right word? from line 15ish to line 19. So that way you can see a complete domain list as well as a bunch of random website endings which is I think what I'm supposed to do for the bonus. I logged in the console the concantenation of what's returned from the for loops + randomWeb. I created a an element list within the HTML by using JS. I defined the li text content with what the for loops return. With the appendChild, I added this li element to the ul element in my HTML, making it visible on the webpage. Did I understand that right?

--- src/app.js
+++ src/app.js
@@ -11,13 +11,15 @@ window.onload = function() {
   let adjs = ['uptopian', 'dangerous', 'wacky', 'odd'];
   let nouns = ['dumpster', 'spoon', 'violin', 'megaphone'];
   const domains = document.querySelector("ul");
-
+  const webs = ['.com', '.net', '.org', '.io'];
+ 
   for(let i = 0; i < pronouns.length; i++) {
     for(let j = 0; j < adjs.length; j++) {
       for(let k = 0; k < nouns.length; k++) {
-        console.log(pronouns[i] + adjs[j] + nouns[k] + '.com');
+        let randomWeb = webs[Math.floor(Math.random() * webs.length)];
+        console.log(pronouns[i] + adjs[j] + nouns[k] + randomWeb);
         let li = document.createElement("li");
-        li.textContent = pronouns[i] + adjs[j] + nouns[k] + '.com';
+        li.textContent = pronouns[i] + adjs[j] + nouns[k] + randomWeb;
         domains.appendChild(li);
       }
     }
